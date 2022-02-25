9d18094d7839450ec540d36c8e75926dabbd064b
Alyne <alynefae2428@gmail.com>
2022-02-25 13:44:32 -0300

    cadastro e login

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -1,14 +1,14 @@
 let usuario = document.querySelector('#id_usuario')
-
+let labelUsuario = document.querySelector('#labelUsuario')
 
 let email = document.querySelector('#id_email')
-
+let labelEmail = document.querySelector('#labelEmail')
 
 let senha = document.querySelector('#id_senha')
-
+let labelSenha = document.querySelector('#labelSenha')
 
 let confirmarSenha = document.querySelector('#id_confirmarSenha')
-
+let labelConfirmarSenha = document.querySelector('#labelConfirmarSenha')
 
 
 senha.addEventListener('keyup', () => {
