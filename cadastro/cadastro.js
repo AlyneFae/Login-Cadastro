868285ce8abb2f4d6925db676a13ef55c2253cf2
Alyne <alynefae2428@gmail.com>
2022-02-26 19:01:27 -0300

    cadastro e login

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -12,19 +12,63 @@ let labelConfirmarSenha = document.querySelector('#labelConfirmarSenha')
 
 
 senha.addEventListener('keyup', () => {
-  if(senha.value.length <= 7){
-    
+  
+  if(senha.value.length == 0 ){
+
+    labelSenha.setAttribute('style', 'color: black')
+    labelSenha.innerHTML = 'Senha:'
+
+  } 
+  else if(senha.value.length <= 7){
+
+    labelSenha.setAttribute('style', 'color: red')
+    labelSenha.innerHTML = 'Senha: *insira no minimo 8 caracteres'
+
+  }else{
+
+    labelSenha.setAttribute('style', 'color: green')
+    labelSenha.innerHTML = 'Senha:'
+
   }
+
+  const event = new Event('keyup')
+  confirmarSenha.dispatchEvent(event)
+
 })
 
+confirmarSenha.addEventListener('keyup', () => {
+  
+  if(confirmarSenha.value.length == 0){
+
+    labelConfirmarSenha.setAttribute('style', 'color: black')
+    labelConfirmarSenha.innerHTML = 'Confirmar Senha:'
+
+  }
+  else if(confirmarSenha.value != senha.value){
+
+    labelConfirmarSenha.setAttribute('style', 'color: red')
+    labelConfirmarSenha.innerHTML = 'Confirmar Senha: *As senhas não conferem'
+
+  }else{
+
+    labelConfirmarSenha.setAttribute('style', 'color: green')
+    labelConfirmarSenha.innerHTML = 'Confirmar Senha:'
+
+  }
+
+})
+
+function cadastro(){
 
 
 
-
+}
 
 function senhaRevelada() {
+  
     let s = document.getElementById("id_senha");
     let sc = document.getElementById("id_confirmarSenha");
+
     if (s.type === "password") {
       s.type = "text";
       sc.type ="text";
@@ -32,4 +76,5 @@ function senhaRevelada() {
       s.type = "password";
       sc.type = "password";
     }
+
 }
