e01b5ebc80afc802c9476df7d68fa720b9558c67
Alyne <alynefae2428@gmail.com>
2022-02-28 14:00:42 -0300

    cadastro e login

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -1,16 +1,57 @@
+// variaveis de usuario
 let usuario = document.querySelector('#id_usuario')
 let labelUsuario = document.querySelector('#labelUsuario')
+let validUsuario = false
 
+//variaveis de email
 let email = document.querySelector('#id_email')
 let labelEmail = document.querySelector('#labelEmail')
+let validEmail = false
 
+//variaveis de senha 
 let senha = document.querySelector('#id_senha')
 let labelSenha = document.querySelector('#labelSenha')
+let validSenha = false
 
+//variaveis de confirmar senha 
 let confirmarSenha = document.querySelector('#id_confirmarSenha')
 let labelConfirmarSenha = document.querySelector('#labelConfirmarSenha')
+let validConfirmarSenha = false
 
+// divs ocultas
+let msgError = document.querySelector('#msgError')
+let msgSuccess = document.querySelector('#msgSuccess')
 
+// validar o campo de usuario
+usuario.addEventListener('keyup', () => {
+
+  if(usuario.value.length == 0){
+
+    validUsuario = false
+
+  }else{
+
+    validUsuario = true 
+
+  }
+})
+
+// para validar o campo de email
+email.addEventListener('keyup', () => {
+
+  if(email.value.length == 0){
+
+    validEmail = false
+
+  }else{
+
+    validEmail = true 
+
+  }
+  
+})
+
+//para aceitar na senha no minimo 8 caracteres na senha
 senha.addEventListener('keyup', () => {
   
   if(senha.value.length == 0 ){
@@ -23,11 +64,13 @@ senha.addEventListener('keyup', () => {
 
     labelSenha.setAttribute('style', 'color: red')
     labelSenha.innerHTML = 'Senha: *insira no minimo 8 caracteres'
+    validSenha = false
 
   }else{
 
-    labelSenha.setAttribute('style', 'color: green')
+    labelSenha.setAttribute('style', 'color: black')
     labelSenha.innerHTML = 'Senha:'
+    validSenha = true
 
   }
 
@@ -36,6 +79,7 @@ senha.addEventListener('keyup', () => {
 
 })
 
+//para verificar se as senhas são compativeis 
 confirmarSenha.addEventListener('keyup', () => {
   
   if(confirmarSenha.value.length == 0){
@@ -48,33 +92,63 @@ confirmarSenha.addEventListener('keyup', () => {
 
     labelConfirmarSenha.setAttribute('style', 'color: red')
     labelConfirmarSenha.innerHTML = 'Confirmar Senha: *As senhas não conferem'
+    validConfirmarSenha = false
 
   }else{
 
-    labelConfirmarSenha.setAttribute('style', 'color: green')
+    labelConfirmarSenha.setAttribute('style', 'color: black')
     labelConfirmarSenha.innerHTML = 'Confirmar Senha:'
+    validConfirmarSenha = true 
 
   }
 
 })
 
+// Botão de cadastro !!
 function cadastro(){
 
+  if(validUsuario && validEmail && validSenha && validConfirmarSenha){
 
+    //modal sucesso
+    Swal.fire({
+
+      title: 'Sucesso!',
+      text: 'Cadastro efetuado com sucesso',
+      icon: 'success',
+
+    })
+
+  }else{
+
+    //modal erro
+    Swal.fire({
+
+      title: 'Erro!',
+      text: 'Preencha todos os campo corretamente',
+      icon: 'error',
+
+    })
+
+  }
 
 }
 
+// para revelar a senha  
 function senhaRevelada() {
   
     let s = document.getElementById("id_senha");
     let sc = document.getElementById("id_confirmarSenha");
 
-    if (s.type === "password") {
+    if (s.type === "password"){
+
       s.type = "text";
       sc.type ="text";
+
     } else {
+
       s.type = "password";
       sc.type = "password";
+
     }
 
 }
